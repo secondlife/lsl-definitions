0f258388282dac1ad8c15871d53fd112e61ff76b
Builder's Brewery <buildersbrewery@users.noreply.github.com>
2025-12-13 14:41:15 +0100

    Update validate.py

--- validate.py
+++ validate.py
@@ -9,24 +9,24 @@ import yaml
 import jsonschema
 
 
-def jsonschema() -> bool:
-    with open('lsl_definitions.schema.json', 'r', encoding='utf-8') as schema_file:
+def validate_lsl_definitions_via_jsonschema() -> bool:
+    with open("lsl_definitions.schema.json", "r", encoding="utf-8") as schema_file:
         schemadata = json.load(schema_file)
-    with open('lsl_definitions.yaml', 'r', encoding='utf-8') as yaml_file:
+    with open("lsl_definitions.yaml", "r", encoding="utf-8) as yaml_file:
         yamldata = yaml.safe_load(yaml_file)
 
     try:
         jsonschema.validate(instance=yamldata, schema=schemadata)
-        print(f"\nSUCCESS: Valid against the schema")
+        print("\nSUCCESS: Valid against the schema")
         return True
     except jsonschema.exceptions.ValidationError as e:
-        print(f"\nVALIDATION ERROR: INVALID", file=sys.stderr)
+        print("\nVALIDATION ERROR: INVALID", file=sys.stderr)
         print(f"  Message: {e.message}", file=sys.stderr)
         print(f"  Path: {' -> '.join(map(str, e.path))}", file=sys.stderr)
         print(f"  Schema Path: {' -> '.join(map(str, e.schema_path))}", file=sys.stderr)
         return False
     except jsonschema.exceptions.SchemaError as e:
-        print(f"\nSCHEMA ERROR: The schema is invalid", file=sys.stderr)
+        print("\nSCHEMA ERROR: The schema is invalid", file=sys.stderr)
         print(f"  Message: {e.message}", file=sys.stderr)
         return False
     except Exception as e:
