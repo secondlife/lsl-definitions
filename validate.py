7e49c706565c9b1b803ea5a3cbff7a991a469501
Builder's Brewery <buildersbrewery@users.noreply.github.com>
2025-12-16 13:30:09 +0100

    Update validate.py

--- validate.py
+++ validate.py
@@ -9,7 +9,7 @@ import yaml
 import jsonschema
 
 
-def validate_lsl_definitions_via_jsonschema() -> bool:
+def validate_lsl_definitions_via_jsonschema() -> None:
     with open("lsl_definitions.schema.json", "r", encoding="utf-8") as schema_file:
         schemadata = json.load(schema_file)
     with open("lsl_definitions.yaml", "r", encoding="utf-8") as yaml_file:
@@ -18,17 +18,16 @@ def validate_lsl_definitions_via_jsonschema() -> bool:
     try:
         jsonschema.validate(instance=yamldata, schema=schemadata)
         print("\nSUCCESS: Valid against the schema")
-        return True
     except jsonschema.exceptions.ValidationError as e:
         print("\nVALIDATION ERROR: INVALID", file=sys.stderr)
         print(f"  Message: {e.message}", file=sys.stderr)
         print(f"  Path: {' -> '.join(map(str, e.path))}", file=sys.stderr)
         print(f"  Schema Path: {' -> '.join(map(str, e.schema_path))}", file=sys.stderr)
-        return False
+        raise e
     except jsonschema.exceptions.SchemaError as e:
         print("\nSCHEMA ERROR: The schema is invalid", file=sys.stderr)
         print(f"  Message: {e.message}", file=sys.stderr)
-        return False
+        raise e
     except Exception as e:
         print(f"\nOh no ...: {e}", file=sys.stderr)
-        return False
+        raise e
