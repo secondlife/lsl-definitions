780a482a60c1a02eecaf22eedb280bc3d0cecb83
Builder's Brewery <buildersbrewery@users.noreply.github.com>
2025-12-18 15:58:49 +0100

    Switch to jsonschema for definition validation
    updates the workflow that produces test output so that the YAML file is checked against the schema first
    uses JSON Schema's format instead of yamale's so we can
    
        use LSP with LSP-json to validate the schema
        and use LSP with LSP-yaml to validate YAML code
    
    adds an option to validate the YAML file locally against the schema

---
+++ validate.py
@@ -0,0 +1,33 @@
+#!/bin/env python3
+# coding: utf-8
+
+
+import json
+import sys
+import yaml
+
+import jsonschema
+
+
+def validate_lsl_definitions_via_jsonschema() -> None:
+    with open("lsl_definitions.schema.json", "r", encoding="utf-8") as schema_file:
+        schemadata = json.load(schema_file)
+    with open("lsl_definitions.yaml", "r", encoding="utf-8") as yaml_file:
+        yamldata = yaml.safe_load(yaml_file)
+
+    try:
+        jsonschema.validate(instance=yamldata, schema=schemadata)
+        print("\nSUCCESS: Valid against the schema")
+    except jsonschema.exceptions.ValidationError as e:
+        print("\nVALIDATION ERROR: INVALID", file=sys.stderr)
+        print(f"  Message: {e.message}", file=sys.stderr)
+        print(f"  Path: {' -> '.join(map(str, e.path))}", file=sys.stderr)
+        print(f"  Schema Path: {' -> '.join(map(str, e.schema_path))}", file=sys.stderr)
+        raise e
+    except jsonschema.exceptions.SchemaError as e:
+        print("\nSCHEMA ERROR: The schema is invalid", file=sys.stderr)
+        print(f"  Message: {e.message}", file=sys.stderr)
+        raise e
+    except Exception as e:
+        print(f"\nOh no ...: {e}", file=sys.stderr)
+        raise e
