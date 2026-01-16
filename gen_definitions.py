21790b602ef76586512de32caad96561fe9ed54e
tapple <tapplek@gmail.com>
2026-01-16 09:34:53 -0800

    Revert "Reordering needed to binary-match keywords_lsl_78aaa42a-9d1f-b7bb-ac9a-856a2de7b3da.llsd.xml"
    This reverts commit 00acc8f5da1cd3094f8fccdbe303ddc34ceee0d5.

--- gen_definitions.py
+++ gen_definitions.py
@@ -147,12 +147,12 @@ class LSLConstant(NamedTuple):
     def to_dict(self) -> dict:
         return _remove_worthless(
             {
-                "value": _escape_python(self.value),
+                "deprecated": self.deprecated,
                 # Will always use a <string> node, but that's fine for our purposes.
                 # That's already the case for vector and hex int constants, anyway.
-                "type": str(self.type),
                 "tooltip": self.tooltip,
-                "deprecated": self.deprecated,
+                "type": str(self.type),
+                "value": _escape_python(self.value),
             }
         )
 
@@ -176,17 +176,17 @@ class LSLEvent:
     def to_dict(self) -> dict:
         return _remove_worthless(
             {
+                "deprecated": self.deprecated,
                 "arguments": [
                     {
                         a.name: {
-                            "type": str(a.type),
                             "tooltip": a.tooltip,
+                            "type": str(a.type),
                         }
                     }
                     for a in self.arguments
                 ],
                 "tooltip": self.tooltip,
-                "deprecated": self.deprecated,
             }
         )
 
@@ -239,22 +239,21 @@ class LSLFunction:
     def to_dict(self, include_internal: bool = False) -> dict:
         return _remove_worthless(
             {
-                "energy": self.energy,
-                "bool_semantics": self.bool_semantics or False,
                 "arguments": [
                     {
                         a.name: {
-                            "type": str(a.type),
                             "tooltip": a.tooltip,
+                            "type": str(a.type),
                         }
                     }
                     for a in self.arguments
                 ],
-                "return": str(self.ret_type),
-                "god-mode": self.god_mode,
-                "tooltip": self.tooltip,
                 "deprecated": self.deprecated,
+                "energy": self.energy,
+                "god-mode": self.god_mode,
+                "return": str(self.ret_type),
                 "sleep": self.sleep,
+                "tooltip": self.tooltip,
                 **(
                     {}
                     if not include_internal
@@ -550,7 +549,7 @@ def dump_syntax(definitions: LSLDefinitions, pretty: bool = False) -> bytes:
     if pretty:
         return llsd.format_pretty_xml(syntax, indent=3, c_compat=True, sort_maps=False)
     else:
-        return llsd.format_xml(syntax, c_compat=True, sort_maps=False)
+        return llsd.format_xml(syntax, c_compat=True, sort_maps=True)
 
 
 def _write_if_different(filename: str, data: Union[bytes, str]):
