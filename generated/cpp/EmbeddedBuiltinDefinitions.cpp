0e967b076bc4ac94670491ea4f4c1f11e2e359a1
tapple <tapplek@gmail.com>
2026-07-12 07:54:09 -0700

    generate global functions

--- generated/cpp/EmbeddedBuiltinDefinitions.cpp
+++ generated/cpp/EmbeddedBuiltinDefinitions.cpp
@@ -12,54 +12,36 @@ namespace Luau
 
 static constexpr const char* kBuiltinDefinitionBaseSrc = R"BUILTIN_SRC(
 
-@checked declare function require(target: any): any
-
-@checked declare function getfenv(target: any): { [string]: any }
-
 declare _G: any
 declare _VERSION: string
-
-declare function gcinfo(): number
-
-declare function print<T...>(...: T...)
-
-declare function type<T>(value: T): string
-declare function typeof<T>(value: T): string
-
--- `assert` has a magic function attached that will give more detailed type information
-declare function assert<T>(value: T, errorMessage: string?): T
+declare function assert<T>(value: T?, errorMessage: string?): T -- magic type
+declare function dangerouslyexecuterequiredmodule(f: (...any) -> ...any): ...any
 declare function error<T>(message: T, level: number?): never
-
-declare function tostring<T>(value: T): string
-declare function tonumber<T>(value: T, radix: number?): number?
-
-declare function rawequal<T1, T2>(a: T1, b: T2): boolean
-declare function rawget<K, V>(tab: {[K]: V}, k: K): V?
-declare function rawset<K, V>(tab: {[K]: V}, k: K, v: V): {[K]: V}
-declare function rawlen<K, V>(obj: {[K]: V} | string): number
-
-declare function setfenv<T..., R...>(target: number | (T...) -> R..., env: {[string]: any}): ((T...) -> R...)?
-
+declare function gcinfo(): number
+-- declare function getmetatable<T>(obj: T): getmetatable<T> -- builtin
 declare function ipairs<V>(tab: {V}): (({V}, number) -> (number?, V), {V}, number)
-
-declare function pcall<A..., R...>(f: (A...) -> R..., ...: A...): (boolean, R...)
-
--- FIXME: The actual type of `xpcall` is:
--- <E, A..., R1..., R2...>(f: (A...) -> R1..., err: (E) -> R2..., A...) -> (true, R1...) | (false, R2...)
--- Since we can't represent the return value, we use (boolean, R1...).
-declare function xpcall<E, A..., R1..., R2...>(f: (A...) -> R1..., err: (E) -> R2..., ...: A...): (boolean, R1...)
-
--- `select` has a magic function attached to provide more detailed type information
-declare function select<A...>(i: string | number, ...: A...): ...any
-
--- FIXME: This type is not entirely correct - `loadstring` returns a function or
--- (nil, string).
-declare function loadstring<A...>(src: string, chunkname: string?): (((A...) -> any)?, string?)
-
 @checked declare function newproxy(mt: boolean?): any
-
--- Cannot use `typeof` here because it will produce a polytype when we expect a monotype.
+-- declare function next<K, V>(t: {[K]: V}, i: K?): (K?, V) -- builtin
+-- declare function pairs<K, V>(t: {[K]: V}): (({[K]: V}, K?) -> (K?, V), {[K]: V}, K) -- builtin
+declare function pcall<A..., R...>(f: (A...) -> R..., ...: A...): (boolean, R...)
+declare function print<T...>(...: T...): ()
+declare function rawequal<T1, T2>(a: T1, b: T2): boolean
+declare function rawget<K, V>(t: {[K]: V}, k: K): V?
+declare function rawlen<K, V>(t: {[K]: V} | string): number
+declare function rawset<K, V>(t: {[K]: V}, k: K, v: V): {[K]: V}
+@checked declare function require(target: any): any -- magic type
+declare function select<A...>(i: string | number, ...: A...): ...any -- magic type
+-- declare function setmetatable<T, MT>(t: T, mt: MT): setmetatable<T, MT> -- builtin, magic type
+declare function tonumber(value: string? | number, base: number?): number?
+declare function toquaternion(value: string? | quaternion): quaternion?
+declare function torotation(value: string? | quaternion): quaternion?
+declare function tostring<T>(value: T): string
+declare function touuid(val: string? | buffer | uuid): uuid?
+declare function tovector(val: string? | vector): vector?
+declare function type<T>(obj: T): string
+declare function typeof<T>(obj: T): string
 declare function unpack<V>(tab: {V}, i: number?, j: number?): ...V
+declare function xpcall<E, A..., R1..., R2...>(f: (A...) -> R1..., err: (E) -> R2..., ...: A...): (boolean, R1...)
 
 )BUILTIN_SRC";
 
--- lsl_definitions/generators/slua_embedded.py
+++ lsl_definitions/generators/slua_embedded.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import io
 from string import Template
 
 from lsl_definitions.generators.base import register
@@ -13,29 +14,22 @@ def gen_slua_embedded_defs(
 ) -> str:
     """Generate SLua Embedded Definitions File."""
 
-    # quaternion_module = slua_definitions.get_module("quaternion")
-    # rotation_module = SLuaModule(
-    #     name="rotation",
-    #     comment=quaternion_module.comment,
-    #     callable=quaternion_module.callable,
-    #     constants=quaternion_module.constants,
-    #     functions=quaternion_module.functions,
-    # )
-    # slua_definitions.modules.append(rotation_module)
-
-    inserts = {
-        #     SLUA_GLOBAL_FUNCTIONS_REGEX=get_slua_global_functions(slua_definitions),
-        #     SLUA_GLOBAL_MODULES_REGEX=get_slua_modules(slua_definitions),
-        #     SLUA_GLOBAL_CONSTANTS_REGEX=get_slua_global_constants(slua_definitions),
-        #     SLUA_GLOBAL_LSL_CONSTANTS_REGEX=get_LL_constants(slua_definitions),
-        #     SLUA_GLOBAL_LL_MODULE_REGEX=get_LL_module_functions(slua_definitions),
-        #     SLUA_GLOBAL_LL_MODULE_DEPRECATED_REGEX=get_LL_module_deprecated_functions(slua_definitions),
-        #     # nil|string|number|boolean|thread|userdata|symbol|vector|buffer|unknown|never|any
-        #     SLUA_GLOBAL_TYPES_REGEX=get_slua_global_types(slua_definitions),
-        #     SLUA_METAMETHODS_REGEX=get_slua_metamethods(slua_definitions),
-        #     SLUA_GLOBAL_LL_VARIABLE_FUNCTIONS_REGEX=get_LL_variable_functions(slua_definitions),
-        #     SLUA_GLOBAL_LL_VARIABLES_REGEX=get_LL_variables(slua_definitions),
-    }
+    inserts = {}
+    with io.StringIO() as defs:
+        defs.write("declare ")
+        defs.write(slua_definitions.global_constants.pop("_G").to_luau_def())
+        defs.write("\ndeclare ")
+        defs.write(slua_definitions.global_constants.pop("_VERSION").to_luau_def())
+        defs.write("\n")
+        for func in slua_definitions.functions.values():
+            if not func.show_in_syntax_files:
+                continue
+            if func.slua_removed:
+                continue
+            if func.typechecker_flags.builtin:
+                defs.write("-- ")
+            func.write_luau_global_def(defs)
+        inserts["SLUA_GLOBAL_FUNCTIONS"] = defs.getvalue()
 
     with open(template_path) as f:
         template = Template(f.read())
